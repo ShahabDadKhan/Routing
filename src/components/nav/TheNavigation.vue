c00d64ff2d2655aa6074848bbd7528fc78323940
Shahab Dad khan <shahabkhan681@gmail.com>
2021-03-11 11:31:33 +0530

    added navigation

--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -3,10 +3,10 @@
     <nav>
       <ul>
         <li>
-          <router-link to="'/teams">Team</router-link>
+          <router-link to="/teams">Team</router-link>
         </li>
         <li>
-          <router-link to="'/users">Users</router-link>
+          <router-link to="/users">Users</router-link>
         </li>
       </ul>
     </nav>
